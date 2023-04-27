24e453ed1ea965043675d91ca073fc4e8f25cb24
Collin Sakuma <collinmsakuma@gmail.com>
2023-04-26 20:13:47 -0700

    added imports to config and session key

--- server/config.py
+++ server/config.py
@@ -7,12 +7,14 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.secret_key = b"\x84\x98\x85 \x89\x82\xaf'$\xcdD\xa8\x00\xd1?\xe9"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -21,6 +23,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 db = SQLAlchemy(metadata=metadata)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
