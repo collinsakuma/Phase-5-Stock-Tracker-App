f69e659a4271420c6f21467411ddde5f5f9d7e3d
Collin Sakuma <collinmsakuma@gmail.com>
2023-04-26 20:14:40 -0700

    delete table data & add user for password hash checking (it worked)

--- server/seed.py
+++ server/seed.py
@@ -4,14 +4,26 @@
 from random import randint, choice as rc
 
 # Remote library imports
-from faker import Faker
+# from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Stock, WatchedStock, OwnedStock
 
 if __name__ == '__main__':
-    fake = Faker()
+    # fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        User.query.delete()
+        Stock.query.delete()
+        WatchedStock.query.delete()
+        OwnedStock.query.delete()
+
+        user_1 = User(
+        username="Collin",
+        )
+        user_1.password_hash = "collinsakuma"
+
+        db.session.add_all([user_1])
+        db.session.commit()
