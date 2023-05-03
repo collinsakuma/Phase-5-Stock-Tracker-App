138650575c15648ffb849ecfb1dfcb48fa7f34ca
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-02 20:48:02 -0700

    added css import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import './components/index.css'
 
 import App from './components/App';
 import { BrowserRouter as Router } from "react-router-dom";
