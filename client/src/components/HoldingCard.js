1b86e9fb8638372e5768d68c6497d1fb70c90896
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-02 20:43:13 -0700

    add ticker Price live update need to move chart out and to watchlist

--- client/src/components/HoldingCard.js
+++ client/src/components/HoldingCard.js
@@ -36,8 +36,18 @@ function HoldingCard({ holding }) {
         data: []
     }]);
 
+    const [price, setPrice] = useState(0);
+    const [open, setOpen] = useState(0);
+
+    // stock one minute price for candle stick chart
     async function getStockPrice() {
         const response = await fetch(`${API}historical-chart/1min/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}&from=${today}`);
+        return response.json();
+    }
+
+    // stock real time price for ticker information
+    async function getStockQuote() {
+        const response = await fetch(`${API}quote/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}`);
         return response.json();
     }
 
@@ -69,10 +79,35 @@ function HoldingCard({ holding }) {
         };
     }, []);
 
+    useEffect(() => {
+      let timeoutId;
+
+      async function getCurrentPrice() {
+        try {
+          const stockData = await getStockQuote();
+          setPrice(stockData[0].price.toFixed(2));
+          setOpen(stockData[0].open)
+        } catch(error) {
+          console.log(error);
+        }
+        timeoutId = setTimeout(getCurrentPrice, 3000);
+      }
+      getCurrentPrice();
+
+      return () => {
+        clearTimeout(timeoutId);
+      }
+    },[])
+
     return (
         <div>
             <p>start to build out charts {stock.ticker}</p>
             <Chart options={chart.options} series={series} type="candlestick" width="80%" height={320}/>
+            {/* <p style={price > prevPrice ? {color:"green"} : {color: "red"}}>{price}</p> */}
+            <div style={price > open ? {color:"green"} : {color: "red"}}>
+              {price}
+            </div>
+            <p>{open}</p>
         </div>
     )
 }
