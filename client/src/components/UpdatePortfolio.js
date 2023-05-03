b7bd0dbe23951fc1200b7d94ce5514f10769e964
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-02 20:44:56 -0700

    functionality complete. I think?

--- client/src/components/UpdatePortfolio.js
+++ client/src/components/UpdatePortfolio.js
@@ -1,5 +1,5 @@
 import { Button, Form, Label } from 'semantic-ui-react';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const API = "https://financialmodelingprep.com/api/v3/"
 
@@ -8,10 +8,13 @@ function UpdatePortfolio({ user }) {
     const [ticker, setTicker] = useState("");
     const [foundStock, setFoundStock] = useState([]);
     const [companyName, setCompanyName] = useState("");
-    const [quote, setQuote] = useState([])
-    const [quantity, setQuantity] = useState(0)
-    const [stockId, setStockId] = useState(0)
-    const [canSubmit, setCanSubmit] = useState(true)
+    const [quote, setQuote] = useState(0);
+    const [quantity, setQuantity] = useState(0);
+    const [stockId, setStockId] = useState(0);
+    const [canSubmit, setCanSubmit] = useState(true);
+    // state for update stock
+    const [numSharesOwned, setNumSharesOwned] = useState(0);
+    const [numSharesToSell, setNumSharesToSell] = useState(0);
 
     // check if form has valid quote and quanity entered before allowing form submission
     useEffect(() => {
@@ -19,6 +22,19 @@ function UpdatePortfolio({ user }) {
             setCanSubmit(false)
         }
     },[quote, quantity])
+
+    //reset state on form selection (clear forms)
+    const resetState = () => {
+        setFoundStock([]);
+        setTicker("");
+        setCompanyName("");
+        setQuote(0);
+        setStockId(0);
+        setQuantity(0);
+        setCanSubmit(true);
+        setNumSharesOwned(0);
+        setNumSharesToSell(0);
+    }
 
     // Add form information and post stock to Stocks table
     const handleTickerSearch = (e) => {
@@ -30,7 +46,7 @@ function UpdatePortfolio({ user }) {
     useEffect(() => {
         if (foundStock.length !== 0) {
             setCompanyName(foundStock[0].name);
-            setQuote(foundStock[0].price)
+            setQuote(foundStock[0].price);
             let newStock = {
                 ticker: `${(foundStock[0].symbol.toLowerCase())}`,
                 company_name: `${foundStock[0].name}`,
@@ -50,7 +66,7 @@ function UpdatePortfolio({ user }) {
                         .then((r) => r.json())
                         .then((r) => {
                             const matchingStock = r.find((stock) => stock.ticker === ticker.toLowerCase())
-                            setStockId(matchingStock.id)
+                            setStockId(matchingStock.id);
                         })
                 }
                 return r.json();
@@ -70,7 +86,8 @@ function UpdatePortfolio({ user }) {
             stock_id: stockId,
             quantity: quantity,
             bought_total: (quantity * quote),
-            sold_total: 0 
+            sold_total: 0,
+            share_price: quote
         }
         fetch('/transactions', {
             method: "POST",
@@ -81,13 +98,107 @@ function UpdatePortfolio({ user }) {
         })
         // update an existing OwnedStock if user already owns stock or create new OwnedStock instance
         fetch(`/stocks_by_user_id`)
+            .then((r) => r.json())
+            .then((r) => {
+                console.log(r, "response")
+                console.log(stockId)
+                let ownedStock = r.find((stock) => stock.stock_id === stockId);
+                console.log(ownedStock,"ownedStocks")
+                if (ownedStock === undefined) {
+                    let newOwnedStock = {
+                        user_id: user.id,
+                        stock_id: stockId,
+                        quantity: quantity,
+                        total_cost: (quantity * quote)
+                    }
+                    fetch('/owned_stocks', {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(newOwnedStock)
+                    })
+                }
+                if (ownedStock !== undefined) {
+                    console.log("run update")
+                    let update = {
+                        quantity: parseFloat(ownedStock.quantity) + parseFloat(quantity),
+                        total_cost: ownedStock.total_cost + (quantity * quote)
+                    }
+                    fetch(`/owned_stocks/${ownedStock.id}`, {
+                        method: "PATCH",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(update)
+                    })
+                }
+            })
+        // reset state
+        setFoundStock([]);
+        setCompanyName("");
+        setQuantity(0);
+        setQuote(0);
+        setStockId(0);
+        setTicker("");
+    }
+
+    // Sell Form Code
+
+    const handleTickerSearchSell = (e) => {
+        e.preventDefault();
+        fetch(`${API}quote/${ticker}?apikey=${process.env.REACT_APP_API_KEY}`)
+            .then((r) => r.json())
+            .then(setFoundStock)
+    }
+
+    useEffect(() => {
+        fetch('/stocks_by_user_id')
+            .then((r) => r.json())
+            .then((r) => {
+                let ownedStock = r.find((stock) => stock.stock_id === stockId)
+                if (ownedStock !== undefined) {
+                    setNumSharesOwned(ownedStock.quantity)
+                }
+            })
+    },[handleTickerSearchSell])
+
+    const handleSetNumSharesToSell = (e) => {
+        if (e.target.value <= numSharesOwned && e.target.value >= 0) {
+            setNumSharesToSell(parseInt(e.target.value));
+        }
+    }
+
+    const handleSellStocks = () => {
+        // add new sell transaction
+        let newTransaction = {
+            // user state passed down from App
+            user_id: user.id,
+            stock_id: stockId,
+            quantity: numSharesToSell,
+            bought_total: 0,
+            sold_total: (numSharesToSell * quote),
+            share_price: quote 
+        }
+        fetch('/transactions', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTransaction)
+        })
+        // find the ownedStock instance and either update or delete it. 
+        fetch(`/stocks_by_user_id`)
         .then((r) => r.json())
         .then((r) => {
-            let ownedStock = r.find((stock) => stock.id === stockId)
-            if (ownedStock !== undefined) {
+            let ownedStock = r.find((stock) => stock.stock_id === stockId);
+            console.log("here")
+            console.log(numSharesOwned, numSharesToSell);
+            if (numSharesToSell < numSharesOwned) {
+                console.log("update ownedStock")
                 let update = {
-                    quantity: parseFloat(ownedStock.quantity) + parseFloat(quantity),
-                    total_cost: ownedStock.total_cost + (quantity * quote)
+                    quantity: parseFloat(ownedStock.quantity) - parseFloat(numSharesToSell),
+                    // total_cost: ownedStock.total_cost + (quantity * quote)
                 }
                 fetch(`/owned_stocks/${ownedStock.id}`, {
                     method: "PATCH",
@@ -97,27 +208,33 @@ function UpdatePortfolio({ user }) {
                     body: JSON.stringify(update)
                 })
             }
-            if (ownedStock === undefined) {
-                let newOwnedStock = {
-                    user_id: user.id,
-                    stock_id: stockId,
-                    quantity: quantity,
-                    total_cost: (quantity * quote)
-                }
-                fetch('/owned_stocks', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(newOwnedStock)
+            if (numSharesToSell === numSharesOwned) {
+                console.log("delete ownedStock")
+                fetch(`/owned_stocks/${ownedStock.id}`, {
+                    method: "DELETE",
                 })
             }
         })
+        .then(resetState());
+        // setFoundStock([]);
+        // setCompanyName("");
+        // setNumSharesOwned(0);
+        // setNumSharesToSell(0);
+        // setQuote([]);
+        // setStockId(0);
+        // setTicker("");
     }
+
     return (
         <div>
-            <Button onClick={() => setNewOrUpdate(true)}>Buy</Button>
-            <Button onClick={() => setNewOrUpdate(false)}>Sell</Button>
+            <Button onClick={() => {
+                setNewOrUpdate(true);
+                resetState();
+                }}>Buy</Button>
+            <Button onClick={() => {
+                setNewOrUpdate(false);
+                resetState();
+                }}>Sell</Button>
             <div>
                 {newOrUpdate ? (
                     <>
@@ -179,7 +296,68 @@ function UpdatePortfolio({ user }) {
                     </>
                 ) : (
                     <>
-                        <p>update position</p>
+                        <Form onSubmit={handleTickerSearchSell}>
+                            <Form.Field>
+                                <Form.Input 
+                                    name="ticker"
+                                    type="text"
+                                    value={ticker.toUpperCase()}
+                                    onChange={(e) => setTicker(e.target.value)}
+                                    placeholder="ticker"
+                                />
+                            </Form.Field>
+                            <Button type="submit">Search</Button>
+                        </Form>
+                        <Form onSubmit={handleSellStocks}>
+                            <Form.Field>
+                                <Form.Input 
+                                    name="company"
+                                    type="text"
+                                    value={companyName}
+                                    placeholder="Company Name"
+                                    readOnly
+                                />
+                            </Form.Field>
+                            <Form.Field>
+                                <Label>Share Price</Label>
+                                <Form.Input 
+                                    name="quote"
+                                    type="number"
+                                    value={quote}
+                                    onChange={(e) => setQuote(e.target.value)}
+                                    placeholder="Share Price"
+                                    readOnly
+                                />
+                            </Form.Field>
+                            <Form.Field>
+                                <Label>Number of Shares Owned</Label>
+                                <Form.Input 
+                                    name="sharesOwned"
+                                    type="number"
+                                    value={numSharesOwned}
+                                    readOnly
+                                />
+                            </Form.Field>
+                            <Form.Field>
+                                <Form.Input 
+                                    name="sellAmount"
+                                    type="number"
+                                    value={numSharesToSell}
+                                    onChange={(e) => handleSetNumSharesToSell(e)}
+                                />
+                            </Form.Field>
+                            <Form.Field>
+                                <Label>Total Price</Label>
+                                <Form.Input 
+                                    name="sellTotal"
+                                    type="string"
+                                    value={`$ ${(numSharesToSell * quote).toFixed(2)}`}
+                                />
+                            </Form.Field>
+                            <Button type="submit">
+                                Confirm
+                            </Button>
+                        </Form>
                     </>
                 )
             }
