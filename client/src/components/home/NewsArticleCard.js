2b0c529e6570e7791f10ae0cda9b5e3ffe7cdc5e
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-07 21:12:18 -0700

    added styling for cards

--- client/src/components/home/NewsArticleCard.js
+++ client/src/components/home/NewsArticleCard.js
@@ -1,18 +1,21 @@
+import { Card } from 'semantic-ui-react';
 
 function NewsArticleCard({ article }) {
     return (
-        <div style={{display:"flex"}}>
-            <div>
-                <img src={article.image}></img>
-            </div>
-            <div>
-                <div style={{opacity: "50%"}}>
-                    <p>Article From: {article.site}</p>
+        <Card style={{width: "60%", boxShadow:"none"}}>
+            <Card.Content style={{display:"flex"}}>
+                <div style={{marginRight: "25px"}}>
+                    <img className="article-image" src={article.image}></img>
                 </div>
-                <a className="articles-link" href={article.url} target="_blank">{article.title}</a>
-                <p>{article.text}</p>
-            </div>
-        </div>
+                <div>
+                    <div style={{opacity: "50%", marginBottom: "10px"}}>
+                        <p>Article From: {article.site}</p>
+                    </div>
+                    <a className="articles-link" href={article.url} target="_blank">{article.title}</a>
+                    <p className="article-text" style={{marginTop:"5px"}}>{article.text}</p>
+                </div>
+            </Card.Content>
+        </Card>
     )
 }
 export default NewsArticleCard;
