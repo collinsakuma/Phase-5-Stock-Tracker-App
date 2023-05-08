ba5dee433a5823ca2dc85a3e5c1a71d3e5ffe617
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-07 21:18:54 -0700

    added data table to both

--- client/src/components/WatchListCardClosed.js
+++ client/src/components/WatchListCardClosed.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 import Chart from 'react-apexcharts';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const API = "https://financialmodelingprep.com/api/v3/"
 const today = new Date().toISOString().slice(0, 10); // Get today's date in "YYYY-MM-DD" format
@@ -30,6 +36,11 @@ const chart = {
     },
   };
 
+// material UI Table
+function createData(one, two) {
+    return {one, two};
+    }
+
 function WatchListCardClosed({ watchedStock, handleRemoveFromWatchList }) {
     // get stock information from holding through its table relationship
     const stock = watchedStock.stock;
@@ -40,19 +51,28 @@ function WatchListCardClosed({ watchedStock, handleRemoveFromWatchList }) {
     }]);
 
     const [price, setPrice] = useState(0);
+    const [open, setOpen] = useState(0);
     const [companyName, setCompanyName] = useState("");
     const [dollarPriceChange, setDollarPriceChange] = useState(0);
     const [changesPercentage, setChangePercentage] = useState(0);
+    const [prevClose, setPrevClose] = useState(0);
+    const [pe, setPe] = useState(0);
+    const [twoHundredDayAvg, setTwoHundredDayAvg] = useState(0);
+    const [volume, setVolume] = useState(0);
+    const [avgVolume, setAvgVolume] = useState(0);
+
+    const rows = [
+        createData("Previous Close", prevClose.toFixed(2), "PE", pe),
+        createData("Open", open.toFixed(2)),
+        createData("200 Day Average", twoHundredDayAvg.toFixed(2)),
+        createData("PE", pe),
+        createData("Volume", volume.toLocaleString('en-US')),
+        createData("Average Volumne", avgVolume.toLocaleString('en-US'))
+    ]
 
     // stock one minute price for candle stick chart
     async function getStockPrice() {
         const response = await fetch(`${API}historical-chart/5min/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}&from=${fiveDaysAgoFormatted}`);
-        return response.json();
-    }
-
-    // stock real time price for ticker information
-    async function getStockQuote() {
-        const response = await fetch(`${API}quote/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}`);
         return response.json();
     }
 
@@ -65,7 +85,7 @@ function WatchListCardClosed({ watchedStock, handleRemoveFromWatchList }) {
                 for (let i = 0; i < 79; i ++) {
                     dataPrices.push(chartData[i])
                 }
-                console.log(dataPrices)
+                // console.log(dataPrices)
                 const prices = dataPrices.map((instance, index) => ({
                 x: new Date(instance.date),
                 y: [chartData[index].open, chartData[index].high, chartData[index].low, chartData[index].close]
@@ -84,28 +104,62 @@ function WatchListCardClosed({ watchedStock, handleRemoveFromWatchList }) {
         fetch(`${API}quote/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}`)
         .then((r) => r.json())
         .then((r) => {
-            console.log(r)
+            // console.log(r)
             setPrice(r[0].price);
+            setOpen(r[0].open);
             setCompanyName(r[0].name);
-            setDollarPriceChange(r[0].change)
-            setChangePercentage(r[0].changesPercentage)
+            setDollarPriceChange(r[0].change);
+            setChangePercentage(r[0].changesPercentage);
+            setPrevClose(r[0].previousClose);
+            setPe(r[0].pe);
+            setTwoHundredDayAvg(r[0].priceAvg200);
+            setVolume(r[0].volume);
+            setAvgVolume(r[0].avgVolume);
         })
     },[])
     
     return (
-        <div>
-            <div>
-                <h3>{companyName}</h3>
-                <div style={{display: "inline-block"}}>
-                    <h4>{price.toFixed(2)}</h4>
-                    <p style={dollarPriceChange < 0 ? {color:"red"} : {color:"green"}}>{(dollarPriceChange).toFixed(2)}</p>
-                    <p style={changesPercentage < 0 ? {color:"red"} : {color:"green"}}>{`(${(changesPercentage).toFixed(2)}%)`}</p>
+        <Card style={{ width: "80%" }}>
+            <Card.Content>
+                <div style={{display: "flex"}}>
+                    <h2>{companyName}</h2>
+                    <Button className='watchlist-card-button' onClick={() => handleRemoveFromWatchList(watchedStock.id)}>Remove From Watchlist</Button>
                 </div>
-            </div>
-            <p>{watchedStock.stock.ticker}</p>
-            <Button onClick={() => handleRemoveFromWatchList(watchedStock.id)}>X</Button>
-            <Chart options={chart.options} series={series} type="candlestick" width="80%" height={320}/>
-        </div>
+                <div style={{ display: "flex"}}>
+                    <p className="price">{price.toFixed(2)}</p>
+                    <div className="price-movement" style={{display:"flex"}}>
+                    <p className="price-margins" style={dollarPriceChange < 0 ? {color:"red"} : {color:"green"}}>{(dollarPriceChange).toFixed(2)}</p>
+                    <p className="price-margins" style={changesPercentage < 0 ? {color:"red"} : {color:"green"}}>{`(${(changesPercentage).toFixed(2)}%)`}</p>
+                    </div>
+                </div>
+                <div style={{display: "flex", marginTop:"20px"}}>
+                    <div style={{marginRight: "20px"}}>
+                        <h3>Summary</h3>
+                        <TableContainer component={Paper}>
+                            <Table size="small" aria-label="simple table">
+                                <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="left">{row.one}</TableCell>
+                                    <TableCell align="right">{row.two}</TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                    <div style={{flex: "1"}}>
+                        <Chart options={chart.options} series={series} type="candlestick" width="100%" height={320}/>
+                    </div>
+                </div>   
+            </Card.Content>
+        </Card>
 
     )
 }
