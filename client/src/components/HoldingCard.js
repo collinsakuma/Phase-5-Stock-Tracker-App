240cf7e6f29356635df1d3cea99aed8432da63da
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-07 21:16:07 -0700

    holding card styles added

--- client/src/components/HoldingCard.js
+++ client/src/components/HoldingCard.js
@@ -1,49 +1,18 @@
 import { useState, useEffect } from 'react';
-import Chart from 'react-apexcharts';
+import { Card } from 'semantic-ui-react';
 
 
 const API = "https://financialmodelingprep.com/api/v3/"
-const today = new Date().toISOString().slice(0, 10);
-
-// chart formating
-const chart = {
-    options: {
-        chart: {
-        type: 'candlestick',
-        height: 350
-      },
-      title: {
-        text: 'CandelStick Chart',
-        align: 'left'
-      },
-      xaxis: {
-        type: 'datetime'
-      },
-      yaxis: {
-        tooltip: {
-          enabled:true
-        }
-      }
-    },
-  };
 
 function HoldingCard({ holding }) {
     // get stock information from holding through its table relationship
     const stock = holding.stock;
     const stockTicker = stock.ticker.toUpperCase()
 
-    const [series, setSeries] = useState([{
-        data: []
-    }]);
-
     const [price, setPrice] = useState(0);
     const [open, setOpen] = useState(0);
-
-    // stock one minute price for candle stick chart
-    async function getStockPrice() {
-        const response = await fetch(`${API}historical-chart/1min/${stockTicker}?apikey=${process.env.REACT_APP_API_KEY}&from=${today}`);
-        return response.json();
-    }
+    const [change, setChange] = useState(0);
+    const [changePercent, setChangePercent] = useState(0);
 
     // stock real time price for ticker information
     async function getStockQuote() {
@@ -51,33 +20,6 @@ function HoldingCard({ holding }) {
         return response.json();
     }
 
-    useEffect(() => {
-        let timeoutId;
-      
-        async function getChartData() {
-            try {
-                const chartData = await getStockPrice();
-                // console.log(chartData)
-                const prices = chartData.map((instance, index) => ({
-                x: new Date(instance.date),
-                y: [chartData[index].open, chartData[index].high, chartData[index].low, chartData[index].close]
-            }));
-            setSeries([{
-                data: prices,
-            }]);
-            } catch(error) {
-                console.log(error)
-            }
-            timeoutId = setTimeout(getChartData, 60000);
-        }
-      
-        getChartData();
-      
-        // Cleanup function
-        return () => {
-            clearTimeout(timeoutId);
-        };
-    }, []);
 
     useEffect(() => {
       let timeoutId;
@@ -85,8 +27,11 @@ function HoldingCard({ holding }) {
       async function getCurrentPrice() {
         try {
           const stockData = await getStockQuote();
-          setPrice(stockData[0].price.toFixed(2));
-          setOpen(stockData[0].open)
+          // console.log(stockData)
+          setPrice(stockData[0].price);
+          setOpen(stockData[0].open);
+          setChange(stockData[0].change);
+          setChangePercent(stockData[0].changesPercentage);
         } catch(error) {
           console.log(error);
         }
@@ -100,15 +45,19 @@ function HoldingCard({ holding }) {
     },[])
 
     return (
-        <div>
-            <p>start to build out charts {stock.ticker}</p>
-            <Chart options={chart.options} series={series} type="candlestick" width="80%" height={320}/>
-            {/* <p style={price > prevPrice ? {color:"green"} : {color: "red"}}>{price}</p> */}
-            <div style={price > open ? {color:"green"} : {color: "red"}}>
-              {price}
-            </div>
-            <p>{open}</p>
-        </div>
+      <Card style={{margin: 0, width: "60%"}}>
+        <Card.Content>
+          <div className="ui grid" style={{display: "flex"}}>
+            <p className="two wide column"><strong>{holding.stock.ticker.toUpperCase()}</strong></p>
+            <p className="three wide column">{holding.stock.company_name}</p>
+            <p className="two wide column holding-card-position">{`$ ${price.toFixed(2)}`}</p>
+            <p className="two wide column holding-card-position" style={change < 0 ? {color:"red"} : {color:"green"}}>{`$ ${(change * holding.quantity).toFixed(2)}`}</p>
+            <p className="two wide column holding-card-position" style={changePercent < 0 ? {color:"red"} : {color:"green"}}>{`${(changePercent).toFixed(2)} %`}</p>
+            <p className="two wide column holding-card-position">{holding.quantity}</p>
+            <p className="two wide column holding-card-position">{`$ ${(holding.quantity * price).toFixed(2)}`}</p>
+          </div>
+        </Card.Content>
+      </Card>
     )
 }
 export default HoldingCard;
