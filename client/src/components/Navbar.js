a77aedce8fdeb0aacff024b3ac52131ec364f817
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-09 15:20:49 -0700

    styles added to nav bar and us rep trading route added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,35 @@
 import { Menu, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-function Navbar({ handleLogout }) {
+function Navbar({ handleLogout, user }) {
     return (
-        <Menu>
-            <Menu.Item name="Home">
-                <NavLink exact to='/'>Home</NavLink>
+        <Menu className='navbar-nav'>
+            <Menu.Item name="Home" style={{marginLeft:"10%"}}>
+                <NavLink className='navbar' exact to='/'>Home</NavLink>
             </Menu.Item>
             <Menu.Item name="Watchlist">
-                <NavLink exact to='/watchlist'>Watchlist</NavLink>
+                <NavLink className='navbar' exact to='/watchlist'>Watchlist</NavLink>
             </Menu.Item>
             <Menu.Item>
-                <NavLink exact to='/holdings'>Holdings</NavLink>
+                <NavLink className='navbar' exact to='/holdings'>Holdings</NavLink>
             </Menu.Item>
             <Menu.Item>
-                <NavLink exact to='/update_portfolio'>Update Portfolio</NavLink>
+                <NavLink className='navbar' exact to='/update_portfolio'>Update Portfolio</NavLink>
             </Menu.Item>
             <Menu.Item>
-                <NavLink exact to='/transactions'>Transactions</NavLink>
+                <NavLink className='navbar' exact to='/transactions'>Transactions</NavLink>
             </Menu.Item>
             <Menu.Item>
-                <NavLink exact to='/senate_trading'>Senate Trading</NavLink>
+                <NavLink className='navbar' exact to='/senate_trading'>U.S. Rep. Trading</NavLink>
             </Menu.Item>
-            <Menu.Item>
-                <Button onClick={handleLogout}>Logout</Button>
-            </Menu.Item>
+            <Menu.Menu position="right">
+                <Menu.Item position="right">
+                    <p>Hello,&nbsp;&nbsp;{user.username}</p>
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Button onClick={handleLogout}>Logout</Button>
+                </Menu.Item>
+            </Menu.Menu>
         </Menu>
     )
 }
